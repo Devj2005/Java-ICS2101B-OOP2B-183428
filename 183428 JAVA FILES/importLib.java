0b113a928ecf5531f8870ceddd1af5da7d14db0e
Devyan Jethwa Amrish <156841724+Devj2005@users.noreply.github.com>
2025-04-29 13:17:46 +0300

    Add files via upload
    this files have explanations in the files itself

---
+++ 183428 JAVA FILES/importLib.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import javax.swing.*;
+
+public class importLib{
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,  "Hello world!");
+    }
+}
