367c6bf437503b3488f1cc51ecf7cd32fba7f37e
Devyan Jethwa Amrish <156841724+Devj2005@users.noreply.github.com>
2025-05-12 15:57:12 +0300

    Changes have been made to user auth
    Week 3 coding challenge

---
+++ 183428 Java files week 3/DataSource.java
@@ -0,0 +1,4 @@
+interface DataSource {
+    public void execute(); // Method to execute a query on the data source
+    // This method can be overridden by subclasses to provide specific implementations
+}
