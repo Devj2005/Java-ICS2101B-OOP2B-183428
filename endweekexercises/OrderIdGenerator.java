0d24f76a06e5b2510af27a41b48341453b7e82ee
Devyan Jethwa A <vrindevj@gmail.com>
2025-06-30 11:12:42 +0300

    orderidgen

--- endweekexercises/OrderIdGenerator.java
+++ endweekexercises/OrderIdGenerator.java
@@ -1,5 +1,12 @@
 package endweekexercises;
 
+import java.util.Random;
+
 public class OrderIdGenerator {
-    
+    public static void main(String[] args){
+        Random r = new Random();
+        String[] orderIds = new String [5];
+        
+        
+    }
 }
