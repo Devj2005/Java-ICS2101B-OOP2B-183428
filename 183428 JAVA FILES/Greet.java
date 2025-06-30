d75cb6ea40cc885255faf7ac545558a0044c493a
Devyan Jethwa Amrish <156841724+Devj2005@users.noreply.github.com>
2025-06-30 23:42:41 +0300

    Delete 183428 JAVA FILES directory

--- 183428 JAVA FILES/Greet.java
+++
@@ -1,9 +0,0 @@
-public class Greet {
-    void greet(){
-        System.out.println("How you doing? ");
-
-    }
-    public static void main(String[] args) {
-        (new Greet()).greet(); // create an instance of greeter and call greet method
-    }
-}
--- 183428 JAVA FILES/importLib.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import javax.swing.*;
-
-public class importLib{
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,  "Hello world!");
-    }
-}
